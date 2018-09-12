6b15016aab901ff219d11f44ca9a4f088ced24d2
duanmuchaoqun <2947824360@qq.com>
2018-09-12 17:37:14 +0800

    sql/demo.sql

--- sql/demo.sql
+++ sql/demo.sql
@@ -10,10 +10,29 @@ Target Server Type    : MYSQL
 Target Server Version : 50720
 File Encoding         : 65001
 
-Date: 2018-09-06 09:39:48
+Date: 2018-09-12 17:37:00
 */
 
 SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for mg_banner
+-- ----------------------------
+DROP TABLE IF EXISTS `mg_banner`;
+CREATE TABLE `mg_banner` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键id',
+  `title` varchar(500) NOT NULL COMMENT '标题',
+  `image_url` varchar(500) NOT NULL COMMENT '图片地址',
+  `link` varchar(500) NOT NULL COMMENT '链接地址',
+  `position` int(11) NOT NULL DEFAULT '0' COMMENT '排序',
+  `creat_time` int(11) NOT NULL COMMENT '创建时间',
+  `update_time` int(11) NOT NULL COMMENT '更新时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of mg_banner
+-- ----------------------------
 
 -- ----------------------------
 -- Table structure for mg_manager_user
